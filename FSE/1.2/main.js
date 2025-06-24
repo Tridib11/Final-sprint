afb231d2ef49b2ddadf73ea4c7a523a8874f78dd
Tridib11 <tridibghosh12345@gmail.com>
2025-06-24 09:47:59 +0530

    Main files added

---
+++ FSE/1.2/main.js
@@ -0,0 +1,11 @@
+let firstName="Tridib"
+let age=18
+let isMarried=true
+
+// if(isMarried){
+//   console.log(firstName+" is married")
+// }else{
+//   console.log(firstName+" is not married")
+// }
+
+return isMarried?console.log("Married"):console.log("Not married")

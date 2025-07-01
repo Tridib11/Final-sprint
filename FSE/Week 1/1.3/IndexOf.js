fd0eca99b9c99343ebb15d8bdb0b8ac7b6cd86af
Tridib11 <tridibghosh12345@gmail.com>
2025-07-01 07:00:47 +0530

    Add IndexOf.js and getLength.js for string index and length functionalities

---
+++ FSE/Week 1/1.3/IndexOf.js
@@ -0,0 +1,7 @@
+function findIndexOf(str,target){
+  console.log("Orginal String ",str)
+  console.log("Index", str.indexOf(target))
+  console.log("Index", str.lastIndexOf(target))
+}
+
+findIndexOf("Hello world world","world")
---
+++ FSE/Week 1/1.3/getLength.js
@@ -0,0 +1,6 @@
+function getLength(str){
+  console.log("Orginal Str",str)
+  console.log("Length",str.length)
+}
+
+getLength("Hello world")

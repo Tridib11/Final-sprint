b63e8edfbfd84a6e175a3be9a8c9fd52d7c52784
Tridib11 <tridibghosh12345@gmail.com>
2025-07-01 07:23:33 +0530

    Add slice.js and substr.js for string slicing functionalities

---
+++ FSE/Week 1/1.3/slice.js
@@ -0,0 +1,6 @@
+function getSlice(str,start,end){
+  console.log("Orginal String",str)
+  console.log("After Slice",str.slice(start,end))
+}
+
+getSlice("Hello world",0,5)
---
+++ FSE/Week 1/1.3/substr.js
@@ -0,0 +1,5 @@
+const value="tridib ghosh"
+let ans=value.substr(2,5) // from index 2 till 5 characters
+let ans2=value.slice(2,5) // index 2 to index 4
+console.log(ans)
+console.log(ans2)

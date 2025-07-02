91c0ed89481f0c7d9d65034c93700d125ae28fb9
Tridib11 <tridibghosh12345@gmail.com>
2025-07-02 09:22:46 +0530

    Java codes added

---
+++ Java-codes/src/OOPS/Basics.java
@@ -0,0 +1,17 @@
+package OOPS;//class OOPS.Test{
+//    public static void printTest(){
+//        System.out.println("Hello");
+//    }
+//}
+
+class Test{
+    public void printTest(){
+        System.out.println("Hello");
+    }
+}
+public class Basics {
+    public static void main(String[] args) {
+        Test t=new Test();
+        t.printTest();
+    }
+}

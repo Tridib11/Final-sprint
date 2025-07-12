0abf38175cb6ce89fa0e9dd60c03c42f2b662c4e
Tridib11 <tridibghosh12345@gmail.com>
2025-07-13 02:01:25 +0530

    Java codes added

---
+++ Java-codes/src/OOPS/Abstraction/Interface/AnimalStuff.java
@@ -0,0 +1,14 @@
+package OOPS.Abstraction.Interface;
+
+public interface AnimalStuff {
+    public void poop();
+}
+
+class Cat implements AnimalStuff{
+
+
+    @Override
+    public void poop() {
+        System.out.println("dkfhoifhoiwhfowd");
+    }
+}

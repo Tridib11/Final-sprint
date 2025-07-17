430b690e4ffc746d9bc484e6ad40a02c59de5f7f
Tridib11 <tridibghosh12345@gmail.com>
2025-07-17 23:39:35 +0530

    Java codes added

--- Java-codes/src/JCF/Custom_Classes/HashSet__testing.java
+++ Java-codes/src/JCF/Custom_Classes/HashSet__testing.java
@@ -2,10 +2,15 @@ package JCF.Custom_Classes;
 import java.util.*;
 public class HashSet__testing {
     public static void main(String[] args) {
+
+        //Stores unique elements in random order
+
         HashSet<Integer> hs=new HashSet<>();
         for(int i=0;i<10;i++){
             hs.add(i);
         }
         System.out.println(hs);
+
+
     }
 }
---
+++ Java-codes/src/JCF/Custom_Classes/Treeset_testing.java
@@ -0,0 +1,18 @@
+package JCF.Custom_Classes;
+import java.util.*;
+public class Treeset_testing {
+    public static void main(String[] args) {
+        //DS that stores unique elements in a sorted order
+
+        Set<Integer> ts=new TreeSet<>();
+        ts.add(3);
+        ts.add(1);
+        ts.add(4);
+        ts.add(-1);
+        System.out.println(ts);
+
+        for(var num:ts){
+            System.out.println(num);
+        }
+    }
+}

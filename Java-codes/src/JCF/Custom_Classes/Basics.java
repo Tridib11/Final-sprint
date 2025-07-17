3238323bfd091c58e09237abf4bcc6855fd1c2cc
Tridib11 <tridibghosh12345@gmail.com>
2025-07-17 22:43:13 +0530

    Java codes added

--- Java-codes/src/JCF/Custom_Classes/Basics.java
+++ Java-codes/src/JCF/Custom_Classes/Basics.java
@@ -3,9 +3,11 @@ package JCF.Custom_Classes;
 class Data{
     public Integer num;
     private String name;
-    Data(int _num,String _name){
+    private InternalData internalData;
+    Data(int _num,String _name,int _revenue){
         this.num=_num;
         this.name=_name;
+        this.internalData=new InternalData(1000);
     }
 
     public void setNum(Integer _num){
@@ -19,14 +21,26 @@ class Data{
     public String getName(){
         return name;
     }
+
+    public int getNum(){
+        return num;
+    }
+}
+
+class InternalData{
+    public Integer revenue;
+    InternalData(Integer _revenue){
+        this.revenue=_revenue;
+    }
 }
 public class Basics {
     public static void main(String[] args) {
-        Data obj1=new Data(12,"Rohan");
-        Data obj2=new Data(14,"Rahul");
+        Data obj1=new Data(12,"Rohan",100000);
+        Data obj2=new Data(14,"Rahul",1000);
 
         obj2.setName("Rohan");
         System.out.println(obj2.getName());
+        System.out.println(obj2.getNum());
 
     }
 }

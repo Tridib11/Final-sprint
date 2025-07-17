e054891c30fa5ba15b95abd6f1c9dfd3a998f7d7
Tridib11 <tridibghosh12345@gmail.com>
2025-07-17 23:23:58 +0530

    Java codes added

--- Java-codes/src/JCF/Custom_Classes/HashSet__testing.java
+++ Java-codes/src/JCF/Custom_Classes/HashSet__testing.java
@@ -2,6 +2,10 @@ package JCF.Custom_Classes;
 import java.util.*;
 public class HashSet__testing {
     public static void main(String[] args) {
-
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<10;i++){
+            hs.add(i);
+        }
+        System.out.println(hs);
     }
 }

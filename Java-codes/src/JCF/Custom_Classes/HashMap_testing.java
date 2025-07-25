c138b38b58d7f6ef3539d5775b46ad8d025c0f74
Tridib11 <tridibghosh12345@gmail.com>
2025-07-26 01:35:22 +0530

    Refactor HashMap_testing.java to comment out unused code and focus on frequency counting of array elements

--- Java-codes/src/JCF/Custom_Classes/HashMap_testing.java
+++ Java-codes/src/JCF/Custom_Classes/HashMap_testing.java
@@ -5,16 +5,24 @@ public class HashMap_testing {
         /*
         Doesnot stores keys in the sorted order O(1)
          */
-        HashMap<Integer,String> map=new HashMap<>();
-        map.put(1,"Raj");
-        map.put(2,"Rohan");
-        map.put(3,"Rahul");
+//        HashMap<Integer,String> map=new HashMap<>();
+//        map.put(1,"Raj");
+//        map.put(2,"Rohan");
+//        map.put(3,"Rahul");
+//
+//        System.out.println(map.get(1));
+//
+//        map.remove(2);
+//        System.out.println(map.size());
+//        System.out.println(map);
 
-        System.out.println(map.get(1));
-
-        map.remove(2);
-        System.out.println(map.size());
-        System.out.println(map);
+        int[] nums= {1,2,3,3,4,4,4};
+        int minElement=Integer.MAX_VALUE,maxElement=-1;
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        System.out.println(map.values());
 
     }
 }

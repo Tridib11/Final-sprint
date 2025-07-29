cab6ed94d7853ca01bcd0e4d64b29c67f3a0eb4c
Tridib11 <tridibghosh12345@gmail.com>
2025-07-29 13:02:46 +0530

    Format Fileread.js for consistency and add comments for clarity

--- FSE/week-1/Fileread.js
+++ FSE/week-1/Fileread.js
@@ -1,4 +1,8 @@
-const fs=require("fs")
+const fs = require("fs");
 fs.readFile("a.txt","utf-8",(err,data)=>{
   console.log(data)
 })
+
+// const data=fs.readFileSync("a.txt","utf-8")
+// console.log(data)
+console.log("Skipped");

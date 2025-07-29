87fdff455e3a3699edc7abf03b0869c6c703b2a2
Tridib11 <tridibghosh12345@gmail.com>
2025-07-29 11:56:20 +0530

    Add Animal class with constructor and speak method

---
+++ FSE/week-1/Animal.js
@@ -0,0 +1,13 @@
+class Animal{
+  constructor(name,legCount,speaks){
+    this.name=name;
+    this.legCount=legCount;
+    this.speaks=speaks;
+  }
+  speak(){
+    console.log("hi there "+this.speaks)
+  }
+}
+
+let dog=new Animal("Dog",4,"Bhow bhow")
+dog.speak()

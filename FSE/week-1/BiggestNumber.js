8021fc3dc3112e653e1281e89bb91ab83463cb36
Tridib11 <tridibghosh12345@gmail.com>
2025-07-29 11:06:35 +0530

    Add BiggestNumber.js and evenArray.js to calculate maximum and print even numbers

---
+++ FSE/week-1/BiggestNumber.js
@@ -0,0 +1,8 @@
+let ans=[7,5,8,9,1];
+
+let max=ans[0];
+for(let i=1;i<ans.length;i++){
+  max=Math.max(max,ans[i]);
+}
+
+console.log(max)
---
+++ FSE/week-1/evenArray.js
@@ -0,0 +1,6 @@
+let ans=[1,2,3,4,5];
+for(let i=0;i<ans.length;i++){
+  if(ans[i]%2==0){
+    console.log(ans[i])
+  }
+}

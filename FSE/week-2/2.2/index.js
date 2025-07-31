3b7ae943a70fad3d5dcaa5ec815e7cae8ddba6c6
Tridib11 <tridibghosh12345@gmail.com>
2025-07-31 14:18:42 +0530

    Add new endpoint to return user information

--- FSE/week-2/2.2/index.js
+++ FSE/week-2/2.2/index.js
@@ -5,6 +5,13 @@ app.get('/',(req,res)=>{
   res.send("Hello world")
 })
 
+app.get("/shit",(req,res)=>{
+  res.json({
+    name:"Tridib",
+    age:21
+  })
+})
+
 app.listen(3000,()=>{
   console.log("Server satrted")
 })

26466bf43159215902ff93d1d0a7d49cdc69c9f8
Tridib11 <tridibghosh12345@gmail.com>
2025-07-31 14:31:28 +0530

    Fix formatting and add missing semicolons in index.js

--- FSE/week-2/2.2/index.js
+++ FSE/week-2/2.2/index.js
@@ -1,26 +1,26 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
 
-app.get('/',(req,res)=>{
-  res.send("Hello world")
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-app.use(express.json())
-app.post("/test",(req,res)=>{
-  console.log(req.headers.authorization)
-  console.log(req.body.msg)
+app.use(express.json());
+app.post("/test", (req, res) => {
+  console.log(req.headers.authorization);
+  console.log(req.body.msg);
   res.send({
-    msg:"Post works"
-  })
-})
+    msg: "Post works",
+  });
+});
 
-app.get("/shit",(req,res)=>{
+app.get("/shit", (req, res) => {
   res.json({
-    name:"Tridib",
-    age:21
-  })
-})
+    name: "Tridib",
+    age: 21,
+  });
+});
 
-app.listen(3000,()=>{
-  console.log("Server satrted")
-})
+app.listen(3000, () => {
+  console.log("Server satrted");
+});

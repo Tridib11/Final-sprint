76e922403ab297c1f480176b48153466ab9015cb
Tridib11 <tridibghosh12345@gmail.com>
2025-08-02 14:54:34 +0530

    Refactor randomGame.py to enhance number guessing logic and track attempts

--- Genai/pythons-basics/randomGame.py
+++ Genai/pythons-basics/randomGame.py
@@ -2,9 +2,25 @@ import random
 
 num=random.randint(1,10)
 
-guess=int(input("Enter a number "))
 
-if num==guess:
-  print("You won")
-else:
-  print(f"You lost the number was {num}")
+# if num==guess:
+#   print("You won")
+# else:
+#   print(f"You lost the number was {num}")
+
+tries = 0
+while True:
+  guess = int(input("Enter a number "))
+  if guess < num:
+    tries += 1
+    print("Try a little higher")
+  elif guess > num:
+    tries += 1
+    print("Try a little lower")
+  else:
+    tries += 1
+    print("You won")
+    print(f"You took {tries} tries")
+    break
+
+  

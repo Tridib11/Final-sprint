6ea44e92305d83d6b37d19e44045de5f9458696f
Tridib11 <tridibghosh12345@gmail.com>
2025-08-05 23:23:51 +0530

    Update tuple example and uncomment index retrieval

--- Genai/pythons-basics/DS/Tuple.py
+++ Genai/pythons-basics/DS/Tuple.py
@@ -1,12 +1,17 @@
-a=(1,2,3) #immutable,ordered, allow duplicate, heterogeneous
+a=(1,2,3,4,5,5,5.5,print(),"Hello") #immutable,ordered, allow duplicate, heterogeneous
 
-for i in a:
-  print(i)
+# for i in a:
+#   print(i)
 
-print(a.index(2))
-print(a.count(1))
+# print(a.index(2))
+# print(a.count(1))
 
-a,b,c,d=(1,2,3,4)
-print(c)
+# a,b,c,d=(1,2,3,4)
+# print(c)
 
-print(type(a))
+# print(type(a))
+
+# a[i]=10
+index=a.index(5)
+print(index)
+# print(a)

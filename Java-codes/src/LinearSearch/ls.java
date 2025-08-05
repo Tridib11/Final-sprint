7615de1cfaf6ff1b3059d29e1801911fc3284a64
Tridib11 <tridibghosh12345@gmail.com>
2025-08-05 13:35:05 +0530

    Java codes added

--- Java-codes/src/LinearSearch/ls.java
+++ Java-codes/src/LinearSearch/ls.java
@@ -1,14 +1,23 @@
 package LinearSearch;
 
 public class ls {
-    public static void main(StringSearch[] args) {
-        int[] arr={1,2,3,4,5,6};
+    public static void main(String[] args) {
+        int[] arr={2,3,4,5,3};
         System.out.println(linearSearch(arr,3));
     }
 
-    private static int linearSearch(int[] arr,int n){
-        for (int i=0;i<arr.length;i++){
-            if(arr[i]==n){
+//    private static int linearSearch(int[] arr,int n){
+//        for (int i=0;i<arr.length;i++){
+//            if(arr[i]==n){
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+    public static int linearSearch(int nums[], int target) {
+		//Your code goes here
+        for(int i:nums){
+            if(i==target){
                 return i;
             }
         }

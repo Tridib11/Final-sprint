c014ac9df29f9845e1567abb12c2e92af2ab6c18
Tridib11 <tridibghosh12345@gmail.com>
2025-08-06 01:20:27 +0530

    Add set operations demonstration with union, intersection, difference, and symmetric difference

---
+++ Genai/pythons-basics/DS/set_fn.py
@@ -0,0 +1,15 @@
+A={1,2,3}
+B={3,4,5}
+
+union_set=A.union(B)
+print(union_set)
+
+
+intersection_Set=A.intersection(B) #common
+print(intersection_Set)
+
+difference=A.difference(B)
+print(difference)
+
+simmetric_diff=A.symmetric_difference(B)
+print(simmetric_diff)

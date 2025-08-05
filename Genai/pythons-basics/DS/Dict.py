e68432aabfb268ba2667848d8f819a1735c311aa
Tridib11 <tridibghosh12345@gmail.com>
2025-08-06 01:34:43 +0530

    Comment out student dictionary and related print statement for clarity

--- Genai/pythons-basics/DS/Dict.py
+++ Genai/pythons-basics/DS/Dict.py
@@ -1,6 +1,17 @@
-student={
-  "name":"John",
-  "age":20
-}
+# student={
+#   "name":"John",
+#   "age":20
+# }
 
-print(student["name"])
+# print(student["name"])
+
+numbers={1:10,2:20,3:30,4:40}
+numbers.update({5:50})#or d[5]=50
+
+# del numbers[30]
+# print(numbers)
+# for i in numbers:
+#   print(i,":",numbers[i])
+
+for i in numbers.values():
+  print(i)

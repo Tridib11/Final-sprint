0eba077957976d6c17300927e53f774616dc8b1c
Tridib11 <tridibghosh12345@gmail.com>
2025-08-06 13:36:13 +0530

    Add health check endpoint with user authentication and input validation

---
+++ FSE/week-3/3.1/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.get("/health-checkup", (req, res) => {
+  const kidneyId = req.query.kidneyId;
+  const username = req.headers.username;
+  const password = req.headers.password;
+
+  if (username != "Tridib" && password != "pass") {
+    res.status(403).json({
+      msg: "User doesnot exist",
+    });
+    return;
+  }
+
+  if (kidneyId != 1 && kidneyId != 2) {
+    res.status(411).json({
+      msg: "Wrong Inputs",
+    });
+    return;
+  }
+
+  res.send("your heart is healthy");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});

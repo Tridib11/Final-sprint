0fd20a5ba4723a91960d888907d40997e0ca14f2
Tridib11 <tridibghosh12345@gmail.com>
2025-08-06 16:47:57 +0530

    Add rate limiter middleware to health check endpoint

--- FSE/week-3/3.1/MiddlewareTesting.js
+++ FSE/week-3/3.1/MiddlewareTesting.js
@@ -25,9 +25,41 @@ function kidneyMiddleware(req, res, next) {
   }
 }
 
-app.get("/health-checkup", userMiddleware, kidneyMiddleware, (req, res) => {
-  res.send("Your kidney health is healthy");
-});
+let clicks = 0;
+let isBlocked = false;
+function rateLimiter(req, res, next) {
+  if (isBlocked) {
+    return res.status(403).json({
+      msg: "Unwanted traffic pleae try after 5 sec",
+    });
+  }
+
+  clicks++;
+
+  if (clicks >= 5) {
+    isBlocked = true;
+
+    setTimeout(() => {
+      clicks = 0;
+      isBlocked = false;
+    }, 5000);
+    return res.status(403).json({
+      msg: "Unwanted traffic pleae try after 5 sec",
+    });
+  } else {
+    next();
+  }
+}
+
+app.get(
+  "/health-checkup",
+  userMiddleware,
+  kidneyMiddleware,
+  rateLimiter,
+  (req, res) => {
+    res.send("Your kidney health is healthy");
+  }
+);
 
 app.listen(3000, (req, res) => {
   console.log("Server has started");

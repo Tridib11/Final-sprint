bb777a955bba78ef25709cf6d136f00cfa268ec9
Tridib11 <tridibghosh12345@gmail.com>
2025-08-06 19:20:36 +0530

    Add Zod validation to health check endpoint and update start script

--- FSE/week-3/3.1/MiddlewareTesting.js
+++ FSE/week-3/3.1/MiddlewareTesting.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 function userMiddleware(req, res, next) {
   const username = req.headers.username;
   const password = req.headers.password;
@@ -25,37 +24,41 @@ function kidneyMiddleware(req, res, next) {
   }
 }
 
-let clicks=0
-let isBlocked=false
-function rateLimiter(_, res, next){
-  
-  if(isBlocked){
+let clicks = 0;
+let isBlocked = false;
+function rateLimiter(_, res, next) {
+  if (isBlocked) {
     return res.status(403).json({
-      msg:"Unwanted traffic pleae try after 5 sec"
-    })
+      msg: "Unwanted traffic pleae try after 5 sec",
+    });
   }
 
   clicks++;
 
-  if(clicks>=5){
-    isBlocked=true
+  if (clicks >= 5) {
+    isBlocked = true;
 
-    setTimeout(()=>{
-      clicks=0
-      isBlocked=false
-    },5000)
+    setTimeout(() => {
+      clicks = 0;
+      isBlocked = false;
+    }, 5000);
     return res.status(403).json({
-      msg:"Unwanted traffic pleae try after 5 sec"
-    })
-  }else{
-    next()
+      msg: "Unwanted traffic pleae try after 5 sec",
+    });
+  } else {
+    next();
   }
-
 }
 
-app.get("/health-checkup", userMiddleware, kidneyMiddleware,rateLimiter ,(req, res) => {
-  res.send("Your kidney health is healthy");
-});
+app.get(
+  "/health-checkup",
+  userMiddleware,
+  kidneyMiddleware,
+  rateLimiter,
+  (req, res) => {
+    res.send("Your kidney health is healthy");
+  }
+);
 
 app.listen(3000, (req, res) => {
   console.log("Server has started");

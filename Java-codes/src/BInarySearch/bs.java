e49635779798efd7d8f9dd561e96eea02994b9fe
Tridib11 <tridibghosh12345@gmail.com>
2025-08-06 23:32:01 +0530

    Java codes added

--- Java-codes/src/BInarySearch/bs.java
+++ Java-codes/src/BInarySearch/bs.java
@@ -2,8 +2,14 @@ package BInarySearch;
 
 public class bs {
     public static void main(String[] args) {
-        int[] arr={2,3,4,15,16};
-        System.out.println(binarySearch(arr,5));
+        int[] arr={12,11,10,9,8,7,6,5,4,3,2,1};
+        int target=8;
+        if(arr[0]<arr[arr.length-1]){
+            System.out.println(binarySearch(arr,target));
+        }else{
+            System.out.println(oabinarySearch(arr,target));
+        }
+
     }
 
     static int binarySearch(int[] arr,int target){
@@ -16,6 +22,30 @@ public class bs {
             }else if(target>arr[mid]){
                 start=mid+1;
             }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    static int oabinarySearch(int[] arr,int target){
+
+        /**
+         * 11/2=5
+         *
+         * 0  1  2  3 4 5 6 7 8 9 10 11
+           12,11,10,9,8,7,6,5,4,3,2, 1 target=8
+         */
+
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target>arr[mid]){
+                end=mid-1;
+            }else if(target<arr[mid]){
+                start=mid+1;
+            }else{
                 return mid;
             }
         }

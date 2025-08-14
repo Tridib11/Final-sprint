6d8c529b183885c86b12fd968c2680c2ec6a01c9
Tridib11 <tridibghosh12345@gmail.com>
2025-08-14 18:01:28 +0530

    Java codes added

--- Java-codes/src/BInarySearch/CeilingOfANumber.java
+++ Java-codes/src/BInarySearch/CeilingOfANumber.java
@@ -2,6 +2,25 @@ package BInarySearch;
 
 public class CeilingOfANumber {
     public static void main(String[] args) {
+        //Smallest element in the array geater or =  target
+        int[] arr={2,3,5,9,14,16,18};
+        int target=15;
+        System.out.println(Ceiling(arr,target));
 
+    }
+
+    private static int Ceiling(int[] arr,int target){
+        int start=0,end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }else if(target>arr[mid]){
+                start=mid+1;
+            }else if(target<arr[mid]){
+                end=mid-1;
+            }
+        }
+        return start;
     }
 }

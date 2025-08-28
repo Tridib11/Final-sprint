a3c53954ecdc36c1cdaddad528cc9b53e4d7f7be
Tridib11 <tridibghosh12345@gmail.com>
2025-08-29 00:03:33 +0530

    Refactor jwtTesting.js to comment out token generation and enhance verification logging

--- FSE/week-3/3.2/jwtTesting.js
+++ FSE/week-3/3.2/jwtTesting.js
@@ -6,5 +6,9 @@ const value={
 
 }
 
-const token=jwt.sign(value,"secret")
-console.log(token)
+// const token=jwt.sign(value,"secret")
+// console.log(token)
+
+const verify=jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVHJpZGliIiwiYWNjb3VudE51bWJlciI6MTIzMTIzLCJpYXQiOjE3NTY0MDU4NDl9.-LYKvms-WuST0vGQDbie7F2DW6DvDEs6GU809nB79KI","secret")
+
+console.log(verify)

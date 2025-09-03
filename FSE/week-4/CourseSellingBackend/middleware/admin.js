574bb45c30975593c462a127cd59bf1d30adf45f
Tridib11 <tridibghosh12345@gmail.com>
2025-09-04 01:30:07 +0530

    refactor: improve admin middleware structure and formatting for clarity

--- FSE/week-4/CourseSellingBackend/middleware/admin.js
+++ FSE/week-4/CourseSellingBackend/middleware/admin.js
@@ -1,5 +1,21 @@
-function adminMiddleware(req,res,next){
+const { Admin } = require("../db/db");
 
+function adminMiddleware(req, res, next) {
+  const username = req.headers.username;
+  const password = req.headers.password;
+
+  Admin.findOne({
+    username,
+    password,
+  }).then((value) => {
+    if (value) {
+      next();
+    } else {
+      res.status(403).json({
+        msg: "Admin doesnot exist",
+      });
+    }
+  });
 }
 
-module.exports=adminMiddleware
+module.exports = adminMiddleware;

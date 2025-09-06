c6994a16592417d9f029fd0a36b18af6672cc9b4
Tridib11 <tridibghosh12345@gmail.com>
2025-09-06 14:54:23 +0530

    Java codes added

--- Java-codes/src/Sorting/BubbleSort.java
+++ Java-codes/src/Sorting/BubbleSort.java
@@ -1,8 +1,30 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr={1,3,4,2,5};
+        int[] arr={1,2,3,4,5};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
 
+    }
+
+    public static void bubble(int[] arr){
+        boolean swapped;
+        for(int i=0;i<arr.length;i++){
+            swapped=false;
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
     }
 }

3628e319fa2ba96d391750378bc9cdf31f75e13f
Tridib11 <tridibghosh12345@gmail.com>
2025-09-09 03:00:37 +0530

    feat: add server setup with routes and IP address handling

--- HHLD/Day2/index.js
+++ HHLD/Day2/index.js
@@ -1,11 +1,13 @@
-const express = require('express');
- const app = express();
- const port = 3000;
+const x=require("./server")
+const express = require("express");
+const app = express();
+const port = 3000;
 
- app.get('/', (req, res) => {
- res.send('Congratulations HHLD Folks!');
- });
+x()
+app.get("/", (req, res) => {
+  res.send("Congratulations HHLD Folks!");
+});
 
- app.listen(port, () => {
- console.log(`Server is listening at http://localhost:${port}`);
- })
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});

9a5a1c84a78a1bedab314f2f938ac0cf9324d323
Tridib11 <tridibghosh12345@gmail.com>
2025-09-11 13:29:26 +0530

    Add package.json for project setup with dependencies and scripts

---
+++ FSE/week-6/6.2/frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [todos,setTodos]=useState([])
+  useEffect(()=>{
+    axios.get("http://localhost:3000/todos").then((res)=>{
+      setTodos(res.data.todos)
+    })
+    
+  },[])
+  return (
+    <div>
+      {todos.map(todo=>{
+        return <div key={todo.id}>
+          <h1>{todo.title}</h1>
+          <h2>{todo.description}</h2>
+        </div>
+      })}
+    </div>
+  )
+}
+
+export default App

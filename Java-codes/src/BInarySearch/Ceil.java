5aa579ab3eb971e7fde742b45db93ccac54d8c36
Tridib11 <tridibghosh12345@gmail.com>
2025-09-18 13:41:44 +0530

    Java codes added

---
+++ Java-codes/src/BInarySearch/Ceil.java
@@ -0,0 +1,24 @@
+package BInarySearch;
+
+public class Ceil {
+    public static void main(String[] args) {
+        int[] arr={3,4,4,7,8,10};
+        int ans=ceil(arr,arr.length,5);
+        System.out.println(ans);
+
+    }
+    public static int ceil(int[] arr,int n,int x){
+        int start=0,end=n-1;
+        int ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>=x){
+                ans=arr[mid];
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+}

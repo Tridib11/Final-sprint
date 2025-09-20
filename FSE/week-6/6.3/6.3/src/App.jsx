59689abde36c3562611bbb7b1a794e7a5b491a7b
Tridib11 <tridibghosh12345@gmail.com>
2025-09-20 14:07:26 +0530

    feat: implement useMemo for multiplication and enhance App component structure

--- FSE/week-6/6.3/6.3/src/App.jsx
+++ FSE/week-6/6.3/6.3/src/App.jsx
@@ -1,14 +1,41 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from "react";
 
 function App() {
-  const[count,setCount]=useState(0)
+  const [add, setAdd] = useState(0);
+  const [minus, setMinus] = useState(0);
+  const multiplication = useMemo(
+    function mutiply() {
+      console.log("**********");
+      return add * 100;
+    },
+    [add]
+  );
   return (
-    <div>      
-      <button onClick={()=>{
-        setCount(count=>count+1)
-      }}>Count is {count}</button>
+    <div>
+      <h1>Learning useMemo</h1>
+
+      <br />
+      {multiplication}
+      <br />
+      <button
+        onClick={() => {
+          setAdd((add) => add + 1);
+        }}
+      >
+        Addition
+      </button>
+      <span>{add}</span>
+      <br />
+      <button
+        onClick={() => {
+          setMinus((minus) => minus - 1);
+        }}
+      >
+        Substraction
+      </button>
+      <span>{minus}</span>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

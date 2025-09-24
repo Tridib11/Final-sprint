900a4b03d7eba4014937d68c8633d1198f5f2825
Tridib11 <tridibghosh12345@gmail.com>
2025-09-24 23:48:33 +0530

    Java codes added

---
+++ Java-codes/src/Arrays/FractionToRecurringDecimal.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class FractionToRecurringDecimal {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int numerator=sc.nextInt();
+        int denominator=sc.nextInt();
+        System.out.println();
+    }
+    private static String solution(int numerator,int denominator){
+
+    }
+}

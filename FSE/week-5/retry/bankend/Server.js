94e314269eb6b7321c40ef08dd8561e07130ed75
Tridib11 <tridibghosh12345@gmail.com>
2025-10-29 18:46:13 +0530

    feat: enable CORS support in the server

--- FSE/week-5/retry/bankend/Server.js
+++ FSE/week-5/retry/bankend/Server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors=require("cors")
 const app = express();
 const { createTodo, updateTodo } = require("./types");
 const { todo } = require("./db");
+
+
+app.use(cors())
 
 app.use(express.json());
 

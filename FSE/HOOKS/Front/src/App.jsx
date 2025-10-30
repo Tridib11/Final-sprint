7d221acceb35ae88167739645b6fd4b8771f8b46
Tridib11 <tridibghosh12345@gmail.com>
2025-10-30 20:57:44 +0530

    feat: implement data fetching in App component and remove StrictMode from main entry

--- FSE/HOOKS/Front/src/App.jsx
+++ FSE/HOOKS/Front/src/App.jsx
@@ -1,9 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+import axios from "axios" 
 
 function App() {
   const[resourceType,setResourceType]=useState('posts')
+  const[items,setItems]=useState([])
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+      try{
+        const res=await axios.get(`https://jsonplaceholder.typicode.com/${resourceType}`)
+        setItems(res.data)
+      }
+      catch(err){
+        console.error(err)
+      }
+    }
+
+
+    fetchData()
+  },[resourceType])
   return (
     <div>
+      <div>
+
       <button onClick={()=>{
         setResourceType('posts')
       }}>Posts</button>
@@ -13,7 +33,15 @@ function App() {
       <button onClick={()=>{
         setResourceType('comments')
       }}>Comments</button>
+      </div>
+
+      <h1>{resourceType}</h1>
+      {items.map((item)=>{
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
     </div>
+
+    
   )
 }
 

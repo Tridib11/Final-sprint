ce410d41c72444af93cc33b937a6f8daf9d73b11
Tridib11 <tridibghosh12345@gmail.com>
2025-10-30 15:10:56 +0530

    feat: add Sampurna class with basic HashMap usage

---
+++ Java-codes/src/HashmapTesting/Sampurna.java
@@ -0,0 +1,30 @@
+package HashmapTesting;
+
+import java.util.*;
+
+public class Sampurna {
+    public static void main(String[] args) {
+//        Map<Integer,Integer> map=new HashMap<>();
+//
+//
+//
+//        for(int i=0;i<7;i++){
+//            map.put(i,i);
+//        }
+//
+//        System.out.println(map);
+
+
+        Map<String, String> map=new HashMap<>();
+
+        map.put("Tridib","Sampurna");
+
+//        System.out.println(map);
+
+        String ans=map.get("Tridib");
+        System.out.println(ans);
+
+
+
+    }
+}

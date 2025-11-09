2cba182992df94975e812d7f483d6669a121ac08
Tridib11 <tridibghosh12345@gmail.com>
2025-11-09 11:14:34 +0530

    refactor: rename Test component to lowercase and simplify header functionality

--- FSE/HOOKS/Front/src/Test.jsx
+++ FSE/HOOKS/Front/src/Test.jsx
@@ -1,30 +1,28 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-function Test() {
+function test() {
   return (
     <div>
-      <HeaderWithButton />
-      <Header title="Tridib" />
+      <Header title="Tridib"/>
+      <HeaderRandom/>
     </div>
-  );
+  )
 }
 
-function HeaderWithButton() {
-  const [name, setName] = useState("Hi my name is Tridib");
-  function changeTitle() {
-    setName("Hi my name is " + Math.random());
-  }
-
-  return (
-    <div>
-      <button onClick={changeTitle}>Click to update the button</button>
-      <Header title={name} />
-    </div>
-  );
+function HeaderRandom(){
+  const[name,setName]=useState("Tridib")
+  return <div>
+    <Header title={name}/>
+      <button onClick={()=>{
+        setName(Math.random)
+      }}>Click me to update</button>
+  </div>
 }
 
-function Header({ title }) {
-  return <div>{title}</div>;
+function Header({title}){
+  return <div>
+    Hi i am {title}
+  </div>
 }
 
-export default Test;
+export default test
